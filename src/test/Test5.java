c2910ba2df7051d64de5657c8c81dd34835597f3
nakjasabal <nakjasabal@naver.com>
2024-01-05 18:21:01 +0900

    .

---
+++ src/test/Test5.java
@@ -0,0 +1,22 @@
+package test;
+
+class MyClass {
+	static void bFunc() {
+		System.out.println("b가 호출됨");
+	}
+}
+public class Test5 {	
+	static void aFunc() {
+		System.out.println("a가 호출됨");
+	}	
+	public static void main(String[] args) {		
+		aFunc();
+		
+		MyClass mc = new MyClass();
+		mc.bFunc();
+		
+		MyClass.bFunc();
+	}
+}
+
+
